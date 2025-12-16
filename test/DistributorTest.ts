2d3a2ca483b4b97359f1bb997dcd6cadbeb36109
Vasily Styagov <styagov.dev@gmail.com>
2025-12-16 16:11:08 +0000

    test: add tests for native coin distribution

--- test/DistributorTest.ts
+++ test/DistributorTest.ts
@@ -5,8 +5,7 @@ import { expect } from "chai";
 const { viem, networkHelpers } = await network.connect();
 const [owner, alice, bob] = await viem.getWalletClients();
 
-
-describe("Distributor", async () => {
+describe("Distribute ERC20", async () => {
     const amountForAlice = 100n;
     const amountForBob = 200n;
     const totalAmount = amountForAlice + amountForBob;
@@ -66,5 +65,72 @@ describe("Distributor", async () => {
         expect(await token.read.balanceOf([alice.account.address])).to.equal(0n);
         expect(await token.read.balanceOf([bob.account.address])).to.equal(0n);
         expect(await token.read.balanceOf([owner.account.address])).to.equal(totalAmount);
+    })
+});
+
+describe("Distribute native coins", async () => {
+    const amountForAlice = 100n;
+    const amountForBob = 200n;
+
+    async function deployFixtures() {
+        const distributor = await viem.deployContract("Distributor", [owner.account.address]);
+
+        return distributor;
+    }
+
+    it("Should distribute tokens to multiple receivers", async () => {
+        const distributor = await networkHelpers.loadFixture(deployFixtures);
+
+        await viem.assertions.balancesHaveChanged(
+            distributor.write.distributeNative(
+                [
+                    [alice.account.address, bob.account.address],
+                    [amountForAlice, amountForBob]
+                ],
+                { value: amountForAlice + amountForBob }
+            ),
+            [
+                { address: alice.account.address, amount: amountForAlice },
+                { address: bob.account.address, amount: amountForBob }
+            ]
+        );
+    })
+
+    it("Should fail when transfered value does not match the total amount", async () => {
+        const distributor = await networkHelpers.loadFixture(deployFixtures);
+
+        let values = [0n, amountForAlice, amountForAlice + amountForBob + 100n];
+
+        for (let value of values) {
+            await viem.assertions.revertWith(
+                distributor.write.distributeNative(
+                    [
+                        [alice.account.address, bob.account.address],
+                        [amountForAlice, amountForBob]
+                    ],
+                    { value }
+                ),
+                "Attached value does not match the total amount."
+            );
+        }
+    })
+
+    it("Should fail when called by non-owner", async () => {
+        const distributor = await networkHelpers.loadFixture(deployFixtures);
+
+        await viem.assertions.revertWithCustomError(
+            distributor.write.distributeNative(
+                [
+                    [alice.account.address, bob.account.address],
+                    [amountForAlice, amountForBob]
+                ],
+                {
+                    value: amountForAlice + amountForBob,
+                    account: alice.account
+                }
+            ),
+            distributor,
+            "OwnableUnauthorizedAccount"
+        );
     })
 });
